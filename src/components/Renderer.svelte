f7f12f52e948ed88434b622c793d975a5739fa67
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-02-23 15:37:23 +0100

    added CSS framework, ikons and a new component

---
+++ src/components/Renderer.svelte
@@ -0,0 +1,22 @@
+<script>
+import { title } from "process";
+
+</script>
+{#if title}
+    <h1 class="text-3xl mb-3">{title}</h1>
+{:else}
+    <h1 class="text-3xl mb-3 text-gray-600">No title</h1>
+{/if}
+<div class="rendered">
+    {#if content}
+        {content}
+    {:else}
+        <p class="text-gray-600">No content</p>
+    {/if}
+</div>
+
+<script>
+    // exporting props
+    export let title = ''
+    export let content = ''
+</script>
