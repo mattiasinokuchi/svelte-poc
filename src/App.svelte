cfad6a239fab5932f87df8c134f29640d0ee4289
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-02-24 13:25:16 +0100

    added markdown rendering

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
-<Renderer title="Hello" content="World!" />
+<!--This is the main component-->
+<Renderer title="Hello" content={content} />
 <script>
-    import Renderer from "./components/Renderer.svelte";
+    import Renderer from "./components/Renderer.svelte"
+    const content = `# Markdown here!
+    Here's a **great** post.`   // Backticks (`) preserves line breaks
 </script>
