e0b2a8d7c69837febc7479ac5400ea7643fa6bdc
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-02-24 16:04:59 +0100

    added comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
+// This is a configuration file for Webpack
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const Dotenv = require('dotenv-webpack')
 const path = require('path')
 
+// Define the production variable if we're building the application for production
 const mode = process.env.NODE_ENV || 'development'
 const prod = mode === 'production'
 
 module.exports = {
-    entry: {
+    entry: {    // Define an entrypoint called bundle, starting from src/main.js
         bundle: [path.resolve(__dirname, 'src/main.js')],
     },
-    resolve: {
+    resolve: {  // Support Svelte modules from NPM
         extensions: ['.mjs', '.js', '.svelte'],
         mainFields: ['svelte', 'browser', 'module', 'main']
     },
-    output: {
+    output: {   // Compiled files (bundle.js) are placed in the public folder
         path: path.resolve(__dirname, 'public'),
         filename: '[name].js',
         chunkFilename: '[name].[id].js'
     },
-    module: {
+    module: {   // Load .svelte and .css files.
         rules: [
             {
                 test: /\.svelte$/,
@@ -41,7 +43,7 @@ module.exports = {
         ]
     },
     mode,
-    plugins: [
+    plugins: [  // Loads the dotenv plugin
         new Dotenv(),
         new MiniCssExtractPlugin({
             filename: '[name].css'
