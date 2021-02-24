ec41105d4e75e8c781b12fcae212508721e20fbb
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-02-24 14:27:05 +0100

    removed type error

--- src/components/ErrorBox.svelte
+++ src/components/ErrorBox.svelte
@@ -1,5 +1,5 @@
 <!--This component displays an error message-->
-<div class="bg-orange-100 border-l-4 border-orange-500   text-orange-700 p-4 my-2 mx-6" role="alert">
+<div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4 my-2 mx-6" role="alert">
     <p class="font-bold">Error</p>
     <p>{err}</p>
 </div>
