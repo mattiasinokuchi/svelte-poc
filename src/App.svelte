64ad8d712a03c4e122d6387c9bcd2fe2dbbe63d4
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-02-25 06:07:45 +0100

    changed component to navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,24 @@
 <!--This is the main component-->
-<Renderer title="Hello" content={content} />
+
+<Navbar />
+
+<div class="container mx-auto w-full lg:w-3/5 px-2 pt-2 mt-2">
+    {#if $view == 'add'}
+        <ViewAdd />
+    {:else if $view && $view.startsWith('view/')}
+        <ViewObject objectId={$view.substring(5)} />
+    {:else}
+        <ViewList />
+    {/if}
+</div>
+
 <script>
-import Renderer from './components/Renderer.svelte'
-const content = `# Markdown here!
-Here's a **great** post.`   // Backticks (`) preserves line breaks
+// Components
+import Navbar from './components/Navbar.svelte'
+import ViewAdd from './components/ViewAdd.svelte'
+import ViewObject from './components/ViewObject.svelte'
+import ViewList from './components/ViewList.svelte'
+
+// Stores
+import {view} from './stores.js'
 </script>
