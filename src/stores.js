79a33e8ab5f8a73d606fc94c73c147ab6390c496
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-02-25 05:36:05 +0100

    added comment

--- src/stores.js
+++ src/stores.js
@@ -3,5 +3,5 @@
 import {writable, derived} from 'svelte/store'
 export const profile = writable(null)
 export const token = writable(null)
-export const isAuthenticated = derived([token, profile], (a) =>   a && a[0] && a[1])
+export const isAuthenticated = derived([token, profile], (a) =>   a && a[0] && a[1]) // read-only derived from other stores
 export const view = writable(null) 
