3890d6a4066200dba6bd31b1dc967763ab86eac2
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-02-25 05:12:39 +0100

    added comments

--- src/components/ViewAdd.svelte
+++ src/components/ViewAdd.svelte
@@ -1,6 +1,7 @@
 <!-- This is a view component with full layout of the page for adding new journal entries  -->
 
-<AddForm on:added={added} bind:content bind:title />
+<AddForm on:added={added} bind:content bind:title />    <!--  two-way binding to AddForm components' properties (updates ViewAdd component upon changes)
+-->
 <div class="bg-white shadow rounded px-8 pt-6 pb-8 mb-4 text-sm">
     <h1 class="text-3xl mb-2">Preview</h1>
     <div class="border border-gray-600 p-4">
