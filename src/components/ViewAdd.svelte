381fcd06aad8af95eb967dafcbfc20c366a2c742
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-02-25 13:29:05 +0100

    replaced view

--- src/components/ViewAdd.svelte
+++ src/components/ViewAdd.svelte
@@ -1,27 +1,26 @@
 <!-- This is a view component with full layout of the page for adding new journal entries  -->
 
 <div in:fade>
-    <AddForm on:added={added} bind:content bind:title />    <!-- two-way binding to AddForm components' properties
-    -->    
-</div>
-<div class="bg-white shadow rounded px-8 pt-6 pb-8 mb-4 text-sm">
-    <h1 class="text-3xl mb-2">Preview</h1>
-    <div class="border border-gray-600 p-4">
-        <Renderer {title} {content} />
+    <AddForm on:added={added} bind:content bind:title />
+    <div class="bg-white shadow rounded px-8 pt-6 pb-8 mb-4 text-sm">
+        <h1 class="text-3xl mb-2">Preview</h1>
+        <div class="border border-gray-600 p-4">
+            <Renderer {title} {content} />
+        </div>
     </div>
 </div>
 
 <script>
+import {fade} from 'svelte/transition'
+import {push} from 'svelte-spa-router'
 import AddForm from './AddForm.svelte'
 import Renderer from './Renderer.svelte'
-import {view} from '../stores.js'
-import {fade} from 'svelte/transition'
 
 let content = ''
 let title = ''
 function added(event) {
     if (event && event.detail && event.detail.objectId) {
-        $view = 'view/' + event.detail.objectId // Stores the object ID in a Svelte store called $view
+        push('/view/' + event.detail.objectId)
     }
 }
 </script>
