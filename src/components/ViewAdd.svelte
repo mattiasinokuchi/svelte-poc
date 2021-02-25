6fdec52176e770b7005999f02cd3847628d810ee
Mattias Inokuchi <46524414+mattiasinokuchi@users.noreply.github.com>
2021-02-25 05:25:56 +0100

    added comments

--- src/components/ViewAdd.svelte
+++ src/components/ViewAdd.svelte
@@ -1,6 +1,6 @@
 <!-- This is a view component with full layout of the page for adding new journal entries  -->
 
-<AddForm on:added={added} bind:content bind:title />    <!--  two-way binding to AddForm components' properties
+<AddForm on:added={added} bind:content bind:title />    <!-- two-way binding to AddForm components' properties
 -->
 <div class="bg-white shadow rounded px-8 pt-6 pb-8 mb-4 text-sm">
     <h1 class="text-3xl mb-2">Preview</h1>
@@ -18,7 +18,7 @@ let content = ''
 let title = ''
 function added(event) {
     if (event && event.detail && event.detail.objectId) {
-        $view = 'view/' + event.detail.objectId
+        $view = 'view/' + event.detail.objectId // Stores the object ID in a Svelte store called $view
     }
 }
 </script>
